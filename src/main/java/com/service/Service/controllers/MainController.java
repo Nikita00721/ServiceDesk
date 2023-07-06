9d8c23c8b2ffa6f3d68491e2eeff3a373bee7b13
Никита Кремнёв <nkremnev3003@gmail.com>
2023-07-06 19:34:46 +0300

    Добавлена функция редактирования статей

--- src/main/java/com/service/Service/controllers/MainController.java
+++ src/main/java/com/service/Service/controllers/MainController.java
@@ -6,8 +6,12 @@ import com.service.Service.repo.RequestTypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class MainController {
@@ -41,6 +45,27 @@ public class MainController {
     @PostMapping("/delete")
     public String deleteRequest(@RequestParam Long requestId) {
         requestTypeRepository.deleteById(requestId);
+        return "redirect:/types";
+    }
+
+    @GetMapping("/{id}/type-edit")
+    public String typeEdit(@PathVariable(value = "id") long id, Model model) {
+        if(!requestTypeRepository.existsById(id)){
+            return "redirect:/types";
+        }
+        Optional<RequestType> post = requestTypeRepository.findById(id);
+        ArrayList<RequestType> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("reqtype", res);
+        return "type-edit";
+    }
+
+    @PostMapping("/{id}/type-edit")
+    public String blogPostUpdate(@PathVariable(value = "id") long id, @RequestParam String name, @RequestParam String description, Model model) {
+        RequestType requestType = requestTypeRepository.findById(id).orElseThrow();
+        requestType.setName(name);
+        requestType.setDescription(description);
+        requestTypeRepository.save(requestType);
         return "redirect:/types";
     }
 }
