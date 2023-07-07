58f5a577bc00b2d8efe4a1c020f2fce4b8daee01
Никита Кремнёв <nkremnev3003@gmail.com>
2023-07-07 18:43:37 +0300

    Реализована кнопка "показать заявки данного типа". Потом сделаю без кнопки

--- src/main/java/com/service/Service/controllers/RequestController.java
+++ src/main/java/com/service/Service/controllers/RequestController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -25,10 +26,34 @@ public class RequestController {
     RequestRepository requestRepository;
 
     @PostMapping("/request-add")
-    public String ReqAdd(@RequestParam String requestType, @RequestParam String fullName,
+    public String ReqAdd(@RequestParam Long requestType, @RequestParam String fullName,
                          @RequestParam String email, @RequestParam String description, Model model) {
-        Request request = new Request(requestType, fullName, email, new Date(), description);
-        requestRepository.save(request);
+        Optional<RequestType> requestTypeOptional = requestTypeRepository.findById(requestType);
+        if (requestTypeOptional.isPresent()) {
+            RequestType selectedRequestType = requestTypeOptional.get();
+
+            Request request = new Request();
+            request.setFullName(fullName);
+            request.setEmail(email);
+            request.setDescription(description);
+            request.setType(selectedRequestType);
+            request.setSubmissionDate(new Date());
+
+            requestRepository.save(request);
+        }
+
         return "redirect:/";
+    }
+
+    @GetMapping("/requests/{requestTypeId}")
+    public String getRequestsByType(@PathVariable Long requestTypeId, Model model) {
+        Optional<RequestType> requestTypeOptional = requestTypeRepository.findById(requestTypeId);
+        if (requestTypeOptional.isPresent()) {
+            RequestType requestType = requestTypeOptional.get();
+            List<Request> requests = requestRepository.findByType(requestType);
+            model.addAttribute("requests", requests);
+            model.addAttribute("requestType", requestType);
+        }
+        return "requests-by-type";
     }
 }
--- src/main/java/com/service/Service/repo/RequestRepository.java
+++ src/main/java/com/service/Service/repo/RequestRepository.java
@@ -1,7 +1,11 @@
 package com.service.Service.repo;
 
+import com.service.Service.models.RequestType;
 import org.springframework.data.repository.CrudRepository;
 import com.service.Service.models.Request;
 
+import java.util.List;
+
 public interface RequestRepository extends CrudRepository<Request, Long> {
+    List<Request> findByType(RequestType type);
 }
