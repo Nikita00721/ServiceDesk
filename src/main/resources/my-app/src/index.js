99d7e84653957fcff10708cbd3c78327d4db65d4
BaoChi-1 <jelvira02@mail.ru>
2023-07-11 00:01:08 +0300

    Маршрутизация

--- src/main/resources/my-app/src/index.js
+++ src/main/resources/my-app/src/index.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import RequestEdit from './RequestEdit';
 import RequestByType from './RequestByType';
 import TypeEdit from './TypeEdit';
 import Types from './Types';
 
+
 ReactDOM.render(
+  <BrowserRouter>
   <Router>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/request-edit/:id" component={RequestEdit} />
-      <Route path="/request-by-type/:id" component={RequestByType} />
-      <Route path="/type-edit/:id" component={TypeEdit} />
-      <Route path="/types" component={Types} />
-    </Switch>
-  </Router>,
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="/request-edit/:id" element={<RequestEdit />} />
+      <Route path="/request-by-type/:id" element={<RequestByType />} />
+      <Route path="/type-edit/:id" element={<TypeEdit />} />
+      <Route path="/types" element={<Types />} />
+    </Routes>
+  </Router>
+  </BrowserRouter>
+  ,
   document.getElementById('root')
 );
